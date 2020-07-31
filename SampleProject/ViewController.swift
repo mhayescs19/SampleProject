870ea20ed0c4e97dec24b56e9d657380978ebb7e
mhayescs19 <mhayescs19@gmail.com>
2020-07-30 19:05:40 -0700

    Initial Commit

---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Michael Hayes on 7/30/20.
+//  Copyright © 2020 Michael Hayes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
